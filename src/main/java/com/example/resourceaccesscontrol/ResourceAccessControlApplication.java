1fa80f1b900182ebd7e9190673761ac978682266
FIRST_NAME LAST_NAME <eliaskkristensen@gmail.com>
2024-03-18 08:26:57 +0100

    oppdatert github

---
+++ src/main/java/com/example/resourceaccesscontrol/ResourceAccessControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.resourceaccesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceAccessControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceAccessControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/resourceaccesscontrol/ResourceAccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resourceaccesscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceAccessControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
